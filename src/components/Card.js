4963222ad53f2188ae11b0fe065214b85c9e05a6
Michael Callahan <CallahanMichaelE@gmail.com>
2020-03-03 13:00:15 -0700

    added dummy data file and links

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,10 +5,18 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Container from "@material-ui/core/Container";
+import Grid from '@material-ui/core/Grid';
+
 
 const useStyles = makeStyles({
+  flex: {
+    width: "100%",
+    display: "flex",
+    flexWrap: "wrap",
+  },
   root: {
-    minWidth: 275,
+    // width:"30%",
   },
   bullet: {
     display: 'inline-block',
@@ -21,6 +29,7 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12,
   },
+
 });
 
 
@@ -29,30 +38,35 @@ export default function RecipeCard(props) {
   
 
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Title: {props.rec.name}
-        </Typography>
-        <Typography variant="h5" component="h2">
-          Source: {props.rec.source}
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Ingredients: {props.rec.ingredients}
-        </Typography>
-        <Typography variant="body2" component="p">
-          Instructions: {props.rec.instructions}
-        </Typography>
-        <Typography variant="body2" component="p">
-          Categories: {props.rec.category}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="medium">Edit</Button>
-      </CardActions>
-      <CardActions>
-        <Button size="medium">Delete</Button>
-      </CardActions>
-    </Card>
+    <Grid container spacing={2}>
+      <Grid item xs={12}></Grid>
+      <Grid item xs>
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+              Title: {props.rec.name}
+            </Typography>
+            <Typography variant="h5" component="h2">
+              Source: {props.rec.source}
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              Ingredients: {props.rec.ingredients}
+            </Typography>
+            <Typography variant="body2" component="p">
+              Instructions: {props.rec.instructions}
+            </Typography>
+            <Typography variant="body2" component="p">
+              Categories: {props.rec.category}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="medium">Edit</Button>
+          </CardActions>
+          <CardActions>
+            <Button size="medium">Delete</Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
