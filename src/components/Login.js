61818ab84c343710ef6cceccb8cb84d4ab396413
Juana Villalpando <juana.v14@gmail.com>
2020-03-02 17:51:24 -0800

    updated routing changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -127,7 +127,7 @@ const Login = props => {
               </Button>
               <Grid container justify="center">
                 <Grid item>
-                  <Link href="/" variant="body2">
+                  <Link href="/register" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
