df3c3e67a71d4f28cf82d9d53c69f97835a65868
Michael Callahan <CallahanMichaelE@gmail.com>
2020-03-05 10:34:42 -0700

    changed secret recipe to a form and updated preset text to info from recipe. Stste also added - it just needs final axios call

--- src/components/SecretRecipe.js
+++ src/components/SecretRecipe.js
@@ -11,15 +11,16 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 // import Container from "@material-ui/core/Container";
 import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles({
     flex: {
       display: "flex",
-      flexWrap: "wrap",
+      justifyContent: "center",
     },
-    root: {
-      // width:"30%",
-    },
+    // root: {
+    //   width:"80%",
+    // },
     bullet: {
       display: 'inline-block',
       margin: '0 2px',
@@ -67,48 +68,117 @@ const handleEdit = e => {
     })
 };
 
+const [editRecipe, setEditRecipe] = useState({
+  name: `${props.location.secretRecipe.name} `,
+  source: `${props.location.secretRecipe.source}`,
+  category: `${props.location.secretRecipe.category}`,
+  instructions: `${props.location.secretRecipe.instructions}`,
+  ingredients: `${props.location.secretRecipe.ingredients}`,
+  // user_id: localStorage.getItem('user_id')
+  // instructions: "",
+});
+
+const handleChange = event => {
+  setEditRecipe({ ...editRecipe, [event.target.name]: event.target.value });
+  console.log("this is the new recipe",editRecipe)
+};
 
 
+const changeEditMode = () => {
+  console.log("should go to edit mode")
+}
 
+ const [rec, setRec]=useState()
 
+//  useEffect((rec)=> {
+//   isInEditMode: false
+//  }, [])
    
     return(
-        <div>
-            <NavBar/>
-<Grid container spacing={2}>
-      <Grid item xs={12}></Grid>
-      <Grid item xs>
-        <Card className={classes.root}>
-          <CardContent>
-            <Typography className={classes.title} color="textSecondary" gutterBottom>
-              Title: {props.location.secretRecipe.name} 
-            </Typography>
-            <Typography variant="h5" component="h2">
-              Source: {props.location.secretRecipe.source}
-            </Typography>
-            {/* <Typography className={classes.pos} color="textSecondary">
-              Ingredients: {props.rec.ingredients}
-            </Typography> */}
-            {/* <Typography variant="body2" component="p">
-              Instructions: {props.rec.instructions}
-            </Typography> */}
-            <Typography variant="body2" component="p">
-              Categories: {props.location.secretRecipe.category}
-            </Typography>
-          </CardContent>
-          <CardActions>
+      <div>
+      <NavBar/>
+      <div >  
+        <Grid container spacing={2} direction="row" justify="center"  >
+          <Grid item xs={12} sm={6}>
+            <form>
+              <TextField 
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Title" 
+                defaultValue={props.location.secretRecipe.name}
+                name="name"
+                autoComplete="name"
+                autoFocus
+                value={editRecipe.name}
+                onChange={handleChange}
+              />
+              <TextField 
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Source"
+                defaultValue={props.location.secretRecipe.source} 
+                name="name"
+                autoComplete="name"
+                autoFocus
+                value={editRecipe.source}
+                onChange={handleChange}
+              />
+              <TextField 
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Ingredients"
+                defaultValue={props.location.secretRecipe.ingredients}
+                name="name"
+                autoComplete="name"
+                autoFocus
+                value={editRecipe.ingredients}
+                onChange={handleChange}
+              />
+              <TextField 
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Instructions"
+                defaultValue={props.location.secretRecipe.instructions} 
+                name="name"
+                autoComplete="name"
+                autoFocus
+                value={editRecipe.instructions}
+                onChange={handleChange}
+              />
+              <TextField 
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Category"
+                defaultValue={props.location.secretRecipe.category} 
+                name="name"
+                autoComplete="name"
+                autoFocus
+                value={editRecipe.category}
+                onChange={handleChange}
+              />
             <Button size="medium">Edit</Button>
             <Link to='/dashboard'>
             <Button size="medium" onClick={handleDelete}>Delete</Button>
             </Link>
-          </CardActions>
-
-          <CardActions>
-        
-          </CardActions>
-        </Card>
+        </form>
       </Grid>
     </Grid>
+        </div>
         </div>
     )
 }
